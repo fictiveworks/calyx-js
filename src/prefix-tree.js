56a2e5fa7b3b29c62872d77b124aaf3da4a020c3
Mark Rickerby <me@maetl.net>
2021-06-09 01:25:28 +1200

    Refactor names and add docs to Prefix Tree

--- src/prefix-tree.js
+++ src/prefix-tree.js
@@ -1,11 +1,23 @@
-class PrefixNode {
+/**
+ * A node in the prefix tree. Leaf nodes point to the index that label lookups
+ * will match on.
+ */
+class IndexNode {
   constructor(children, index) {
     this.children = children;
     this.index = index;
   }
 }
 
-class PrefixEdge {
+/**
+ * An directed edge pointing to a node. The label is the prefix fragment that
+ * must satisfy the constraint of being a common prefix for all branches that
+ * descend from this branch of the tree.
+ *
+ * Wildcard edges are specially marked so that the lookup algorithm knows to
+ * capture the consumed prefix characters and execute the forward match.
+ */
+class LabeledEdge {
   constructor(node, label, isWildcard) {
     this.node = node;
     this.label = label;
@@ -13,6 +25,9 @@ class PrefixEdge {
   }
 }
 
+/**
+ * Result struct for successful label lookups.
+ */
 class MatchResult {
   constructor(label, index, captured) {
     this.label = label;
@@ -21,9 +36,22 @@ class MatchResult {
   }
 }
 
-class Trie {
+/**
+ * Returned from unsuccessful lookups.
+ * @type {null}
+ */
+const EmptyResult = null;
+
+/**
+ * A prefix tree (radix trie) that supports wildcard pattern matching with
+ * substring captures.
+ *
+ * Use `add` to insert new label and index mappings into the tree and `lookup`
+ * or `hasLabel` to search the tree for a given label.
+ */
+class PrefixTree {
   constructor() {
-    this.root = new PrefixNode([], null);
+    this.root = new IndexNode([], null);
   }
 
   add(label, index) {
@@ -52,16 +80,16 @@ class Trie {
           if (prefix == edge.label) {
             // Current prefix matches the edge label exactly so we can continue
             // down the tree without mutating the current branch.
-            nextNode = new PrefixNode([], indexSlot);
-            currentNode.children.push(new PrefixEdge(nextNode, suffix, isWildcard))
+            nextNode = new IndexNode([], indexSlot);
+            currentNode.children.push(new LabeledEdge(nextNode, suffix, isWildcard))
           } else {
             // We have a partial match on current edge so replace it with the
             // new prefix then rejoin the remaining suffix to the existing branch.
             edge.label = edge.label.substring(prefix.length, edge.label.length);
-            nextNode = new PrefixNode([], indexSlot);
-            const prefixNode = new PrefixNode([edge], null);
-            prefixNode.children.push(new PrefixEdge(nextNode, suffix, isWildcard))
-            currentNode.children[j] = new PrefixEdge(prefixNode, prefix, isWildcard);
+            nextNode = new IndexNode([], indexSlot);
+            const adjoiningNode = new IndexNode([edge], null);
+            adjoiningNode.children.push(new LabeledEdge(nextNode, suffix, isWildcard))
+            currentNode.children[j] = new LabeledEdge(adjoiningNode, prefix, isWildcard);
           }
 
           currentNode = nextNode;
@@ -72,13 +100,32 @@ class Trie {
       if (!matchedPrefix) {
         // No existing edges have a common prefix so push a new branch onto the
         // tree at the current level.
-        const nextEdge = new PrefixEdge(new PrefixNode([], indexSlot), part, isWildcard);
+        const nextEdge = new LabeledEdge(new IndexNode([], indexSlot), part, isWildcard);
         currentNode.children.push(nextEdge);
         currentNode = nextEdge.node;
       }
     }
   }
 
+  /**
+   * Checks if the given label maps to an index in the tree.
+   *
+   * @param  {String}  label [string to check for]
+   * @return {Boolean}       [True if the given label exists in the tree]
+   */
+  hasLabel(label) {
+    return this.lookup(label) !== EmptyResult;
+  }
+
+  /**
+   * Searches the tree for an index match on a particular label.
+   *
+   * This is either an exact string lookup or wildcard match, depending on
+   * whether wildcard matching patterns have been added.
+   *
+   * @param  {String} label String to search for
+   * @return {(MatchResult|EmptyResult)}
+   */
   lookup(label) {
     const labelLength = label.length;
     let currentNode = this.root;
@@ -130,10 +177,19 @@ class Trie {
     if (currentNode != null && currentNode.index != null && charsConsumed == labelLength) {
       return new MatchResult(label, currentNode.index, charsCaptured);
     } else {
-      return null;
+      return EmptyResult;
     }
   }
 
+  /**
+   * Returns any string which occurs as the prefix of both a and b.
+   *
+   * If a and b have a different initial character, returns the empty string.
+   *
+   * @param  {String} a
+   * @param  {String} b
+   * @return {String} The common prefix of a and b
+   */
   commonPrefix(a, b) {
     let selectedPrefix = "";
     let index = 0;
@@ -150,4 +206,4 @@ class Trie {
   }
 }
 
-export default Trie;
+export default PrefixTree;
---
+++ test/prefix-tree-test.js
@@ -0,0 +1,89 @@
+import test from 'ava'
+import PrefixTree from '../src/prefix-tree.js'
+
+test("longest common prefix of strings", t => {
+  const trie = new PrefixTree();
+
+  t.is(trie.commonPrefix("a", "b"), "");
+  t.is(trie.commonPrefix("aaaaa", "aab"), "aa");
+  t.is(trie.commonPrefix("aa", "ab"), "a");
+  t.is(trie.commonPrefix("ababababahahahaha", "ababafgfgbaba"), "ababa");
+  t.is(trie.commonPrefix("abab", "abab"), "abab");
+});
+
+test("delete prefix", t => {
+  const string = "testing";
+  const prefix = "te";
+
+  t.is(string.substring(prefix.length, string.length), "sting");
+});
+
+test('insert single value', t => {
+  const trie = new PrefixTree();
+  trie.add("one", 0);
+
+  t.is(trie.lookup("one").index, 0);
+});
+
+test("lookup with literal string value", t => {
+  const trie = new PrefixTree();
+  trie.add("test", 2);
+  trie.add("team", 3);
+
+  t.is(trie.lookup("test").index, 2);
+  t.is(trie.lookup("team").index, 3);
+  t.falsy(trie.lookup("teal"));
+});
+
+test("lookup with leading wildcard", t => {
+  const trie = new PrefixTree();
+  trie.add("%es", 111);
+
+  t.is(trie.lookup("buses").index, 111);
+  t.is(trie.lookup("bushes").index, 111);
+  t.falsy(trie.lookup("bus"));
+  t.falsy(trie.lookup("train"));
+});
+
+test("lookup with trailing wildcard", t => {
+  const trie = new PrefixTree();
+  trie.add("te%", 222);
+
+  t.is(trie.lookup("test").index, 222);
+  t.is(trie.lookup("team").index, 222);
+  t.falsy(trie.lookup("rubbish"));
+  t.falsy(trie.lookup("total"));
+});
+
+test("lookup with anchored wildcard", t => {
+  const trie = new PrefixTree();
+  trie.add("te%s", 333);
+
+  t.is(trie.lookup("tests").index, 333);
+  t.is(trie.lookup("teams").index, 333);
+  t.falsy(trie.lookup("total"));
+  t.falsy(trie.lookup("test"));
+  t.falsy(trie.lookup("team"));
+});
+
+test("lookup with catch all wildcard", t => {
+  const trie = new PrefixTree();
+  trie.add("%", 444);
+
+  t.is(trie.lookup("tests").index, 444);
+  t.is(trie.lookup("teams").index, 444);
+  t.is(trie.lookup("total").index, 444);
+  t.is(trie.lookup("test").index, 444);
+  t.is(trie.lookup("team").index, 444);
+});
+
+test("lookup with cascading wildcard", t => {
+  const trie = new PrefixTree();
+  trie.add("%y", 555);
+  trie.add("%s", 666);
+  trie.add("%", 777);
+
+  t.is(trie.lookup("ferry").index, 555);
+  t.is(trie.lookup("bus").index, 666);
+  t.is(trie.lookup("car").index, 777);
+});
